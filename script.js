9b2e749ca1eb3263ca3e8b9e7deba11d7f75cdc4
Itay coifman <itaycoifman@gmail.com>
2023-05-24 16:14:40 +0300

    minor changes

--- script.js
+++ script.js
@@ -29,51 +29,51 @@ function loadDriveAPI() {
 }
 
 function uploadFile() {
-const fileInput = document.getElementById('fileInput');
-const file = fileInput.files[0];
-if (!file) {
-console.error('No file selected.');
-return;
-}
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+  if (!file) {
+    console.error('No file selected.');
+    return;
+  }
 
-const metadata = {
-name: file.name,
-mimeType: file.type,
-parents: ['1RuWaX7smjSdF7EhM0VGtGsaDJndR4WA6'] // Replace with the desired parent directory ID
-};
+  const metadata = {
+    name: file.name,
+    mimeType: file.type,
+    parents: ['1RuWaX7smjSdF7EhM0VGtGsaDJndR4WA6'] // Replace with the desired parent directory ID
+  };
 
-const reader = new FileReader();
-reader.onload = (e) => {
-const data = e.target.result;
-const boundary = '-------314159265358979323846';
-const delimiter = "\r\n--" + boundary + "\r\n";
-const closeDelimiter = "\r\n--" + boundary + "--";
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const data = e.target.result;
+    const boundary = '-------314159265358979323846';
+    const delimiter = "\r\n--" + boundary + "\r\n";
+    const closeDelimiter = "\r\n--" + boundary + "--";
 
-const requestBody =
-delimiter +
-'Content-Type: application/json\r\n\r\n' +
-JSON.stringify(metadata) +
-delimiter +
-'Content-Type: ' + file.type + '\r\n' +
-'Content-Transfer-Encoding: base64\r\n' +
-'\r\n' +
-data.replace(/^.*?,/, '') +
-closeDelimiter;
+    const requestBody =
+      delimiter +
+      'Content-Type: application/json\r\n\r\n' +
+      JSON.stringify(metadata) +
+      delimiter +
+      'Content-Type: ' + file.type + '\r\n' +
+      'Content-Transfer-Encoding: base64\r\n' +
+      '\r\n' +
+      data.replace(/^.*?,/, '') +
+      closeDelimiter;
 
-const request = gapi.client.request({
-path: '/upload/drive/v3/files',
-method: 'POST',
-params: { uploadType: 'multipart' },
-headers: {
-  'Content-Type': 'multipart/related; boundary=' + boundary,
-},
-body: requestBody
-});
+    const request = gapi.client.request({
+      path: '/upload/drive/v3/files',
+      method: 'POST',
+      params: { uploadType: 'multipart' },
+      headers: {
+        'Content-Type': 'multipart/related; boundary=' + boundary,
+      },
+      body: requestBody
+    });
 
-request.execute((response) => {
-console.log('File uploaded successfully:', response);
-});
-};
+    request.execute((response) => {
+      console.log('File uploaded successfully:', response);
+    });
+  };
 
-reader.readAsDataURL(file);
+  reader.readAsDataURL(file);
 }
