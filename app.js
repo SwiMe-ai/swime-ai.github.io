3788570985ccee6cc36fcf44ae3c1a8ac4f19964
farjon <guyfar@post.bgu.ac.il>
2023-05-30 11:12:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ dashboardModule.config(function($routeProvider) {
  	
  	var SCOPES ='https://www.googleapis.com/auth/drive.file';
  	var CLIENT_ID ='924258763370-vb4af8620mtabrrcsc4o7speg6b5btqn.apps.googleusercontent.com';
- 	var API_KEY = 'AIzaSyA5j7sLOg3G7rKqSZWQH1DV0eRtxZUBRoo';
+ 	var API_KEY = 'AIzaSyChqZRwaJtMFezNwR5LvQs5OgY2N0c57oM';
 
  	$scope.handleAuthClick = function() {
 
